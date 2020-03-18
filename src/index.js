8efa15ab816ead11fba94981a6bb915b484ad5bf
Grant Butler <grantjbutler@users.noreply.github.com>
2020-03-17 20:02:13 -0400

    Start to form this into a program.

--- src/index.js
+++ src/index.js
@@ -1,74 +1,21 @@
+const { isIPv4 } = require('net')
+const os = require('os')
+
+const { program } = require('commander')
+
 const MixerManager = require('./mixer-manager')
 
+program
+    .version('0.0.1')
+    .requiredOption('-i, --ip <ip>', 'Mixer IP address')
+    .option('-p, --web-port <port>', 'Port on which the HTTP API will be served', 8080)
+
+program.parse(process.argv)
+
 let manager = new MixerManager({
-    localAddress: '10.0.0.14',
-    localPort: 12345
+    localAddress: '0.0.0.0',
+    localPort: 57121
 })
-manager.connect('10.0.0.32', (mixer) => {
-    mixer.fadeTo(12, 0.75, 1000)
+manager.connect(program.ip, (mixer) => {
+    // mixer.fadeTo(12, 0.75, 1000)
 })
-
-// const osc = require('osc')
-
-// let udpPort = new osc.UDPPort({
-// 	localAddress: '10.0.0.14',
-// 	localPort: 52361,
-//     remoteAddress: '10.0.0.32',
-//     remotePort: 10024,
-//     metadata: true
-// })
-
-// udpPort.on('raw', (buf) => {
-//     const str = buf.toString('ascii');
-//     console.log(str)
-// })
-
-// udpPort.on('error', error => {
-// 	console.warn('[osc] Error:', error.stack);
-// });
-
-// udpPort.on('open', () => {
-// 	console.info('[osc] Port open, can now communicate with a Behringer X32.');
-// });
-
-// udpPort.on('close', () => {
-// 	console.warn('[osc] Port closed.');
-// });
-
-// udpPort.open()
-
-// // udpPort.send({
-// //     address: '/mix/on'
-// // })
-
-// udpPort.send({
-//     address: '/info'
-// })
-
-// // renewSubscriptions()
-
-// // function renewSubscriptions() {
-// // 	udpPort.send({
-// // 		address: '/batchsubscribe',
-// // 		args: [
-// // 			// First defines the local endpoint that the X32 will send this subscription data to.
-// // 			{type: 's', value: '/chMutes'},
-// // 			{type: 's', value: '/mix/on'},
-// // 			{type: 'i', value: 0},
-// // 			{type: 'i', value: 63},
-// // 			{type: 'i', value: 10}
-// // 		]
-// // 	});
-
-// // 	udpPort.send({
-// // 		address: '/batchsubscribe',
-// // 		args: [
-// // 			// First defines the local endpoint that the X32 will send this subscription data to.
-// // 			{type: 's', value: '/chFaders'},
-// // 			{type: 's', value: '/mix/fader'},
-// // 			{type: 'i', value: 0},
-// // 			{type: 'i', value: 63},
-// // 			{type: 'i', value: 10}
-// // 		]
-// // 	});
-// // }
